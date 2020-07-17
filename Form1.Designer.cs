eb1fb8ce3d3c15b2f67d8dbedff45f85b72328cc
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-07-16 23:49:38 -0400

    design added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnSecond = new System.Windows.Forms.Button();
+            this.btnFourth = new System.Windows.Forms.Button();
+            this.btnFifth = new System.Windows.Forms.Button();
+            this.btnThird = new System.Windows.Forms.Button();
+            this.btnSixth = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(121, 81);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(75, 23);
+            this.btnFirst.TabIndex = 0;
+            this.btnFirst.Text = "First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            // 
+            // btnSecond
+            // 
+            this.btnSecond.Location = new System.Drawing.Point(328, 80);
+            this.btnSecond.Name = "btnSecond";
+            this.btnSecond.Size = new System.Drawing.Size(75, 23);
+            this.btnSecond.TabIndex = 1;
+            this.btnSecond.Text = "Second";
+            this.btnSecond.UseVisualStyleBackColor = true;
+            // 
+            // btnFourth
+            // 
+            this.btnFourth.Location = new System.Drawing.Point(121, 204);
+            this.btnFourth.Name = "btnFourth";
+            this.btnFourth.Size = new System.Drawing.Size(75, 23);
+            this.btnFourth.TabIndex = 2;
+            this.btnFourth.Text = "Fourth";
+            this.btnFourth.UseVisualStyleBackColor = true;
+            // 
+            // btnFifth
+            // 
+            this.btnFifth.Location = new System.Drawing.Point(328, 204);
+            this.btnFifth.Name = "btnFifth";
+            this.btnFifth.Size = new System.Drawing.Size(75, 23);
+            this.btnFifth.TabIndex = 3;
+            this.btnFifth.Text = "Fifth";
+            this.btnFifth.UseVisualStyleBackColor = true;
+            // 
+            // btnThird
+            // 
+            this.btnThird.Location = new System.Drawing.Point(565, 81);
+            this.btnThird.Name = "btnThird";
+            this.btnThird.Size = new System.Drawing.Size(75, 23);
+            this.btnThird.TabIndex = 4;
+            this.btnThird.Text = "Third";
+            this.btnThird.UseVisualStyleBackColor = true;
+            // 
+            // btnSixth
+            // 
+            this.btnSixth.Location = new System.Drawing.Point(565, 204);
+            this.btnSixth.Name = "btnSixth";
+            this.btnSixth.Size = new System.Drawing.Size(75, 23);
+            this.btnSixth.TabIndex = 5;
+            this.btnSixth.Text = "Sixth";
+            this.btnSixth.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(328, 338);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSixth);
+            this.Controls.Add(this.btnThird);
+            this.Controls.Add(this.btnFifth);
+            this.Controls.Add(this.btnFourth);
+            this.Controls.Add(this.btnSecond);
+            this.Controls.Add(this.btnFirst);
+            this.Name = "Form1";
+            this.Text = "Week_8_Lab";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnSecond;
+        private System.Windows.Forms.Button btnFourth;
+        private System.Windows.Forms.Button btnFifth;
+        private System.Windows.Forms.Button btnThird;
+        private System.Windows.Forms.Button btnSixth;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
